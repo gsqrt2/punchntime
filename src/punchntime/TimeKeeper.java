a2ace87bc99cb24f4434f3604d803b2d3784c36d
Giorgos Marios <gsqrt2@gmail.com>
2017-07-18 22:18:39 +0300

    db update and more

--- src/punchntime/TimeKeeper.java
+++ src/punchntime/TimeKeeper.java
@@ -8,7 +8,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.Dimension;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TimeKeeper extends JFrame {
 	
@@ -20,17 +23,17 @@ public class TimeKeeper extends JFrame {
 				{
 					try{
 						long elapsedTime = (System.currentTimeMillis()-startTime)/1000;
-						timeLabel.setText(""+elapsedTime);
+						timeLabel.setText(""+secondsFormat());
 						Thread.sleep(1000);
 					}catch(Exception e){System.out.println("Exception with thread sleeping in updateTimeDisplay: "+e);}
 				}
 			}
 		};
 		initGui();
-		PntConnector connector = new PntConnector();
-		Connection pntConnection = connector.pntConnect();
-		if(pntConnection!=null)
-			System.out.println("db connect!!!!");
+		
+		/*static user id, to delete after login*/
+		userId = 1;
+		projectId = 1;
 	}
 
 	public static void main(String[] args) {
@@ -42,10 +45,16 @@ public class TimeKeeper extends JFrame {
 	private void initGui()
 	{
 		mainPanel = new JPanel(new BorderLayout());
+		timePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		timeLabel = new JLabel("00:00:00");
-		mainPanel.add(timeLabel, BorderLayout.CENTER);
+		timePanel.add(timeLabel);
+		mainPanel.add(timePanel, BorderLayout.CENTER);
 		
+		infoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		
+		projectTaskLabel = new JLabel("project1 - task1");
+		infoPanel.add(projectTaskLabel);
+		mainPanel.add(infoPanel, BorderLayout.PAGE_START);
 		
 		startButton = new JButton("start");
 		stopButton = new JButton("stop");
@@ -85,6 +94,7 @@ public class TimeKeeper extends JFrame {
             }
         });
 		getContentPane().add(mainPanel);
+		//setPreferredSize(new Dimension(250, 120));
 		pack();
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -103,11 +113,14 @@ public class TimeKeeper extends JFrame {
 	
 	private void stopTimeKeeper()
 	{
+		stopTime = System.currentTimeMillis();
 		clockRunning = false;
 		startButton.setEnabled(true);
 		stopButton.setEnabled(false);
 		System.out.println("stopped at: "+new Timestamp(System.currentTimeMillis()).toString());
 		System.out.println("time span: "+(System.currentTimeMillis()-startTime));
+		updateDb(userId, projectId);
+		
 	}
 	
 	private void startTimeDisplay()
@@ -116,13 +129,81 @@ public class TimeKeeper extends JFrame {
 		timeDisplayThread.start();
 	}
 	
-	
+	private String secondsFormat()
+	{
+		long currentTime = System.currentTimeMillis()-startTime;
+		int seconds = (int) (currentTime / 1000) % 60 ;
+		int minutes = (int) ((currentTime / (1000*60)) % 60);
+		int hours   = (int) ((currentTime / (1000*60*60)) % 24);
 
-	private JLabel timeLabel;
-	private JPanel mainPanel, buttonPanel;
+		return String.format(
+			      "%02d:%02d:%02d",
+			      hours, minutes, seconds);
+	}
+	
+	private void updateDb(int userId, int projectId)
+	{
+		if(pntConnector == null)
+			pntConnector = new PntConnector();
+		
+		Connection pntConnection = pntConnector.pntConnect();
+		if(pntConnection!=null)
+		{
+			System.out.println("db connect!!!!");
+			PreparedStatement preparedStatement = null;
+			try
+			{
+				String insertTableSQL = "INSERT INTO pnt_sessions"
+					+ "(pnt_session_user, pnt_session_start, pnt_session_stop, pnt_session_project) VALUES"
+					+ "(?,?,?,?)";
+				preparedStatement = pntConnection.prepareStatement(insertTableSQL);
+				
+				preparedStatement.setInt(1, userId);
+				preparedStatement.setTimestamp(2, new Timestamp(startTime));
+				preparedStatement.setTimestamp(3, new Timestamp(stopTime));
+				preparedStatement.setInt(4, projectId);
+				
+				preparedStatement.executeUpdate();
+				
+				System.out.println("updateDb query executed!");
+				
+			}
+			catch(SQLException e)
+			{
+				System.out.println("Exception inserting session: "+e.getMessage());
+			}
+			finally
+			{
+				if(preparedStatement != null)
+				{
+					try
+					{
+						preparedStatement.close();
+					}catch(SQLException e){System.out.println("Exception closing preparedStatement: "+e.getMessage());}
+				}
+
+				if(pntConnection != null)
+				{
+					try
+					{
+						pntConnection.close();
+					}catch(SQLException e){System.out.println("Exception closing pntConnection: "+e.getMessage());}
+				}
+			}
+		}
+		else
+		{
+			System.out.println("pntConnector could not connect to the database.");
+		}
+	}
+
+	private JLabel timeLabel, projectTaskLabel;
+	private JPanel mainPanel, buttonPanel, timePanel, infoPanel;
 	private JButton startButton, stopButton;
 	private boolean clockRunning = false;
-	private long startTime;
+	private long startTime, stopTime;
 	private Runnable displayUpdateRunnable;
 	private Thread timeDisplayThread;
+	private PntConnector pntConnector;
+	private int userId, projectId;
 }
